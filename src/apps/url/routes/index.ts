d6781704b35aa1d9859d3c607e2e0a47627342cb
Marcos Corona <mcorona.avalos02@gmail.com>
2022-10-17 05:27:08 -0300

    refactor: add type to create url request & response

--- src/apps/url/routes/index.ts
+++ src/apps/url/routes/index.ts
@@ -1,25 +1,33 @@
-import { Router, Request } from 'express';
+import { Router, Request, Response } from 'express';
 import { CreateShortUrlUseCase } from '../../../contexts/url/application/create-short-url-use-case';
 import { Base62DecoratorKeyGenerator } from '../../../contexts/url/infrastructure/key-generator/base62-decorator-key-generator';
 import { SnowflakeKeyGenerator } from '../../../contexts/url/infrastructure/key-generator/snowflake-key-generator';
 import { InMemoryShortUrlRepository } from '../../../contexts/url/infrastructure/persistence/in-memory-short-url-repository';
 import httpStatus from 'http-status';
 
+const baseUrl = `http://localhost:${process.env.PORT}`;
+
 export const router = Router();
 
 const keyGenerator = new Base62DecoratorKeyGenerator(
   new SnowflakeKeyGenerator()
 );
-const repository = new InMemoryShortUrlRepository();
-const useCase = new CreateShortUrlUseCase(keyGenerator, repository);
-const baseUrl = `http://localhost:${process.env.PORT}`;
 
-type CreateShortUrlRequest = {
+const repository = new InMemoryShortUrlRepository();
+const createShortUrlUserCase = new CreateShortUrlUseCase(keyGenerator, repository);
+
+type CreateShortUrlBody = {
   url: string;
 };
 
-router.post('/', async (req: Request<CreateShortUrlRequest>, res) => {
-  const shortUrl = await useCase.execute(req.body.url);
+type CreateShortUrlRequest = Request<any, any, CreateShortUrlBody>
+type CreateShortUrlResponse = {
+  shortUrl: string
+}
+
+router.post('/', async (req: CreateShortUrlRequest, res: Response<CreateShortUrlResponse>) => {
+  const {url} = req.body
+  const shortUrl = await createShortUrlUserCase.execute(url);
   return res
     .status(httpStatus.CREATED)
     .send({ shortUrl: `${baseUrl}/${shortUrl.key}` });
